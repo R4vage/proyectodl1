7459d78285a80ca3a4cdc2fcf40482a0d04e5112
R4vage <julianmarc@gmail.com>
2022-04-24 19:28:39 -0300

    RegisterComponent

---
+++ src/Components/RegisterComponent.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react"
+import "../CSS/LoginComponent.css"
+
+
+export default function RegisterComponent (){
+
+    const [registerData, setRegisterData] = useState({
+        nombre: "",
+        apellido: "",
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (event) => {
+        setRegisterData({ ...registerData, [event.target.name]: event.target.value });
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (registerData.email.length == 0, registerData.password.length == 0, registerData.nombre.length == 0, registerData.apellido.length == 0)
+        {
+            alert("Has fallado :(")  
+        }
+        else {
+            alert("Te has registrado con éxito")  
+            console.log(registerData)
+        }
+    };
+
+    
+    return(
+    <>
+        <div className="Login">
+            <div className="LoginComponent">
+                <h1>Registro</h1>
+                <form className="loginFormulario" onSubmit={handleSubmit}>
+                    <h3 className="loginFormularioSubtitle">Por favor, rellene los campos con sus datos</h3>
+
+                    <label className="loginFormularioInputContainer">
+                        <h3>Nombre:</h3>
+                        <input type="text" name="nombre" className="loginFormularioInput" value={registerData.nombre} onChange={handleChange}/>
+                    </label>
+
+                    <label className="loginFormularioInputContainer">
+                        <h3>Apellido:</h3>
+                        <input type="text" name="apellido" className="loginFormularioInput" value={registerData.apellido} onChange={handleChange}/>
+                    </label>
+
+                    <label className="loginFormularioInputContainer">
+                        <h3>Email:</h3>
+                        <input type="text" name="email" className="loginFormularioInput" value={registerData.email} onChange={handleChange}/>
+                    </label>
+
+                    <label className="loginFormularioInputContainer">
+                        <h3>Contraseña:</h3>
+                        <input type="password" name="password" className="loginFormularioInput" value={registerData.password}  onChange={handleChange}/>
+                    </label>
+
+                    <input type="submit" value="Registrarme" className="loginFormularioSubmit" onClick={console.log()}/>
+
+                </form>
+                <h3 className="loginFormularioRegister">¿Ya estás registrado?<a href="/"> Clickea acá maese!!</a></h3>
+            </div>
+        </div>
+    </>
+    )
+}
