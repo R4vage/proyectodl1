6168dd266e253bd46ec587ce8949e54038c98837
R4vage <julianmarc@gmail.com>
2022-04-24 17:40:13 -0300

    Finalizado estilado y empiezo con mensaje en top.

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -1,34 +1,73 @@
+import React, { useState } from "react"
 import "../CSS/LoginComponent.css"
 
 
 export default function LoginComponent (){
 
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: "",
+        estado:"profesor"
+    })
+    const [loginState, setLoginState] = useState("")
+
+    const handleChange = (event) => {
+        setLoginData({ ...loginData, [event.target.name]: event.target.value });
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(loginData)
+        if (loginData.email=="julianm" & loginData.password=="password123" & loginData.estado=="alumno")
+        {
+            alert("Exito")  
+            setLoginState("exito")
+        }
+        else {
+            alert("Has fallado :(")  
+            setLoginState("fallo")
+        }
+    };
+
+    function MensajeLogin () {
+        if (loginState=="exito") {
+            return <h3 className="loginExito">Ha ingresado exitosamente</h3>}
+        if (loginState=="fallo") { 
+            return <h3 className="loginExito">Datos incorrectos</h3>}
+            
+        }
+    
     return(
-    <div className="Login">
-        <div className="LoginComponent">
-            <h1>Stax Food Admin Login</h1>
-            <form className="loginFormulario">
+    <>
+        <div className="Login">
+           <MensajeLogin/>
+            <div className="LoginComponent">
+                <h1>Stax Food Admin Login</h1>
+                <form className="loginFormulario" onSubmit={handleSubmit}>
+                    <h3 className="loginFormularioSubtitle">Por favor, rellene los campos con sus credenciales únicas</h3>
+                    <label className="loginFormularioInputContainer">
+                        <h3>Email:</h3>
+                        <input type="text" name="email" className="loginFormularioInput" value={loginData.email} onChange={handleChange}/>
+                    </label>
 
-                <label className="loginFormularioInput">
-                    <h2>Email:</h2>
-                    <input type="text" name="Email" />
-                </label>
+                    <label className="loginFormularioInputContainer">
+                        <h3>Contraseña:</h3>
+                        <input type="password" name="password" className="loginFormularioInput" value={loginData.password}  onChange={handleChange}/>
+                    </label>
 
-                <label className="loginFormularioInput">
-                    <h2>Contraseña:</h2>
-                    <input type="text" name="Contraseña" />
-                </label>
+                    <select className="loginFormularioSelect" name="estado" value={loginData.estado} onChange={handleChange}>
+                        <option value="profesor">Profesor</option>
+                        <option value="alumno">Alumno</option>
+                    </select>
 
-                <select className="loginFormularioSelect">
-                    <option value="profesor">Profesor</option>
-                    <option value="alumno">Alumno</option>
-                </select>
-                <h3 className="loginFormularioForgot"><a>Forgot password?</a></h3>
-                <input type="submit" value="Login" />
+                    <h3 className="loginFormularioForgot"><a>Forgot password?</a></h3>
 
-            </form>
-            <h3>¿Todavía no te registraste? Clickea<a> acá</a> maese!!</h3>
+                    <input type="submit" value="Login" className="loginFormularioSubmit" onClick={console.log()}/>
+
+                </form>
+                <h3 className="loginFormularioRegister">¿Todavía no te registraste? Clickea<a> acá</a> maese!!</h3>
+            </div>
         </div>
-    </div>
+    </>
     )
 }
